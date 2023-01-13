0a836f51fb65252af22cdfa1434e837ad3ba620f
ilhom <ilhommamatqulov276@gmail.com>
2023-01-13 19:35:02 +0500

    find_all_users_name.py

--- find_all_users_name.py
+++ find_all_users_name.py
@@ -9,4 +9,11 @@ def find_all_users_name(data: dict)->list:
     Returns:
         list: List containing all the users name.
     """
-    return
+    ans = []
+    users = data['messages']
+    for i in users:
+        if i['type'] == 'service' and i.get('actor_id') not in ans:
+            ans.append(i.get('actor'))
+    return ans
+y = read_data('data/result.json')
+print(find_all_users_name(y))
