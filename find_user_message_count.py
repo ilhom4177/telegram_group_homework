77428afda6c6f2d7c2f410ddd40c09db7243537a
ilhom <ilhommamatqulov276@gmail.com>
2023-01-15 17:07:01 +0500

    find_user_message_count.py

--- find_user_message_count.py
+++ find_user_message_count.py
@@ -11,12 +11,8 @@ def find_user_message_count(data: dict, users_id: str)->dict:
     Returns:
         dict: Number of messages of the users
     """
-    a = 0
-    data = data['messages']
-    for i in data:
-        if i['type'] == 'message' and i['from_id'] == users_id:
-            a += 1
-    return a
-y = read_data('data/result.json')
-user = find_all_users_id(y)
-print(find_user_message_count(y,user[1]))
+    message_count = 0
+    for message in data:
+        if message["user_id"] == users_id:
+            message_count += 1
+    return {"user_id": users_id, "message_count": message_count}
